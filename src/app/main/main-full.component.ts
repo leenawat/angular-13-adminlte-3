63b72b1975e4eb067ecae0db22fb558a91641a73
Leenawat Papahom <leenawat.pa@gmail.com>
2022-01-13 22:50:43 +0700

    close or open sidebar

--- src/app/main/main-full.component.ts
+++ src/app/main/main-full.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-full.component.css']
 })
 export class MainFullComponent implements OnInit {
-  
+  public sidebarMenuOpened = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenuSidebar() {
+    if (this.sidebarMenuOpened) {
+      document.querySelector('body')?.classList.remove('sidebar-open');
+      document.querySelector('body')?.classList.add('sidebar-collapse');
+      this.sidebarMenuOpened = false;
+    } else {
+      document.querySelector('body')?.classList.remove('sidebar-collapse');
+      document.querySelector('body')?.classList.add('sidebar-open');
+      this.sidebarMenuOpened = true;
+    }
   }
 
 }
